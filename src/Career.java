83ec3e1df689ef7b77b0e256869c9d74462b14c1
GabrielaHuerta <huertag@go.lisd.net>
2018-05-16 09:23:23 -0500

    Adding careers to Career and College.

--- src/Career.java
+++ src/Career.java
@@ -10,6 +10,8 @@
 public abstract class Career
 {
 	protected String career;
+	protected boolean needDegree;
+	protected int salary; //in thousands
 	
 	public Career()
 	{
@@ -21,41 +23,176 @@ public abstract class Career
 
 class Doctor extends Career
 {
-	private boolean needDegree = true;
-	private int salary;
+	needDegree = true;
+	salary = 100;
+	
+	public String getCareer()
+	{
+		return "Doctor";
+	}
 }
 
-class Superstar
+class Superstar extends Career
 {
+	needDegree = false;
+	salary = 60;
 	
+	public String getCareer()
+	{
+		return "Superstar";
+	}
 }
 
-class TravelAgent
+class Clown extends Career
 {
+	needDegree = false;
+	salary = 10;
 	
+	public String getCareer()
+	{
+		return "Clown";
+	}
 }
 
-class Teacher
+class Teacher extends Career
 {
+	needDegree = true;
+	salary = 50;
 	
+	public String getCareer()
+	{
+		return "Teacher";
+	}
 }
 
-class Athlete
+class Athlete extends Career
 {
+	needDegree = false;
+	salary = 50;
 	
+	public String getCareer()
+	{
+		return "Athlete";
+	}
 }
 
-class SalesPerson
+class SalesPerson extends Career
 {
+	needDegree = false;
+	salary = 30;
 	
+	public String getCareer()
+	{
+		return "SalesPerson";
+	}
 }
 
-class Accountant
+class Accountant extends Career
 {
+	needDegree = true;
+	salary = 80;
 	
+	public String getCareer()
+	{
+		return "Accountant";
+	}
 }
 
-class Artist
+class Artist extends Career
 {
+	needDegree = false;
+	salary = 20;
 	
+	public String getCareer()
+	{
+		return "Artist";
+	}
+}
+
+class Pilot extends Career
+{
+	needDegree = true;
+	salary = 70;
+	
+	public String getCareer()
+	{
+		return "Pilot";
+	}
+}
+
+class Police extends Career
+{
+	needDegree = true;
+	salary = 60;
+	
+	public String getCareer()
+	{
+		return "Police Officer";
+	}
+}
+
+class Scientist extends Career
+{
+	needDegree = true;
+	salary = 100;
+	
+	public String getCareer()
+	{
+		return "Scientist";
+	}
+}
+
+class CEO extends Career
+{
+	needDegree = true;
+	salary = 90;
+	
+	public String getCareer()
+	{
+		return "CEO";
+	}
+}
+
+class Construction extends Career
+{
+	needDegree = false;
+	salary = 10;
+	
+	public String getCareer()
+	{
+		return "Construction Worker";
+	}
+}
+
+class Farmer extends Career
+{
+	needDegree = false;
+	salary = 40;
+	
+	public String getCareer()
+	{
+		return "Farmer";
+	}
+}
+
+class Makeup extends Career
+{
+	needDegree = false;
+	salary = 30;
+	
+	public String getCareer()
+	{
+		return "Cosmetologist";
+	}
+}
+
+class MrStites extends Career
+{
+	needDegree = true;
+	salary = 1000;
+	
+	public String getCareer()
+	{
+		return "Mr. Stites - a God":
+	}
 }
--- src/Money.java
+++ src/Money.java
@@ -14,24 +14,3 @@ public class Money
 	private int salary;
 	
 }
-
-class Salary
-{
-	private int[] so = {20,30,40,50,60,70,80,90,100};
-	                  // 0  1  2  3  4  5  6  7  8
-	private ArrayList<Integer> salaryOptions = new ArrayList<Integer>(Arrays.asList(so));
-	
-	public ArrayList<Integer> getSalaryOptions()
-	{
-		return salaryoptions;
-	}
-	public Integer getSalary()
-	{
-		if(salaryOptions.size()==0)
-			return 0;
-		int rand = (int)(Math.random()*salaryOptions.size());
-		Integer temp = salaryOptions.get(rand);
-		salaryOptions.remove(rand);
-		return temp;
-	}
-}
