47d1a770c23e18e36e238ea11eda80d58e546685
GabrielaHuerta <39127090+GabrielaHuerta@users.noreply.github.com>
2018-05-21 09:42:10 -0500

    Add files via upload

---
+++ src/Controller.java
@@ -0,0 +1,24 @@
+package application;
+
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class Controller
+{
+	@FXML
+	public void choose2(ActionEvent event)
+	{
+		
+	}
+	
+	public void choose3(ActionEvent event)
+	{
+		
+	}
+	
+	public void choose4(ActionEvent event)
+	{
+		
+	}
+}
