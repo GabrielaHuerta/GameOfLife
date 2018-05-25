0b5c0618b8da651d461fa7723144a3809540bb77
AthiraDom <39146111+AthDom123@users.noreply.github.com>
2018-05-25 09:42:25 -0500

    Action Cards Class

---
+++ ActionCards.java
@@ -0,0 +1,21 @@
+public class ActionCards
+{
+	private String name;
+	private int action;
+	
+	public ActionCards()
+	{
+		name= "";
+		action = 0;
+	}
+	public ActionCards(String n, int a)
+	{
+		n = name;
+		a = action;
+	}
+	
+	public void  displayCards()
+	{
+		Image card = new Image("");
+	}
+}
